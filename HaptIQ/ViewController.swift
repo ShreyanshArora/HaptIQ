b648c9f193572428348b640ea1c90a6240c35e55
Anuj Mishra <am4352@srmist.edu.in>
2025-11-01 14:57:42 +0530

    testing

--- HaptIQ/ViewController.swift
+++ HaptIQ/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let label = UILabel()
+    let button = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        // Label setup
+        label.text = "Hello, UIKit!"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.frame = CGRect(x: 50, y: 200, width: 300, height: 50)
+        view.addSubview(label)
+        
+        // Button setup
+        button.setTitle("Change Text", for: .normal)
+        button.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        view.addSubview(button)
     }
-
-
+    
+    @objc func buttonTapped() {
+        label.text = "You tapped the button!"
+    }
 }
-
