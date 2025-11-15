299359d6252ee0e1b23e3e7f689de8754f95808f
Anuj Mishra <am4352@srmist.edu.in>
2025-11-15 18:56:46 +0530

    new ui created 2 pages only

--- HaptIQ/SceneDelegate.swift
+++ HaptIQ/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let mainVC = JoinRoomViewController()
+        let mainVC = HomeViewController()
         let navController = UINavigationController(rootViewController: mainVC)
         navController.isNavigationBarHidden = true
         window.rootViewController = navController
