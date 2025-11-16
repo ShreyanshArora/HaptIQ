7210c40e62325545e3c52c779b807a71750d5250
Anuj Mishra <am4352@srmist.edu.in>
2025-11-17 01:02:00 +0530

    avatar screen integrated

---
+++ HaptIQ/Models/AvatarModel.swift
@@ -0,0 +1,18 @@
+//
+//  AvatarModel.swift
+//  HaptIQ
+//
+//  Created by Anuj   on 17/11/25.
+
+import Foundation
+
+struct AvatarPage {
+    let imageName: String
+    let title: String
+}
+
+
+
+
+
+
